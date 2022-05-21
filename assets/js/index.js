4f277e080d807a15d72fede7cd2a95c10bf720b0
T1213 <1711917202@qq.com>
2022-05-21 10:17:37 +0800

    完成了首页功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,6 +19,16 @@ function getUserInfo() {
       // 调用 renderAvatar 渲染用户头像
       renderAvatar(res.data);
     },
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    // complete: (res) => {
+    //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    //   if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+    //     //  强制清空 token
+    //     localStorage.removeItem("token");
+    //     // 强制跳转到登录页面
+    //     location.href = "/login.html"
+    //   }
+    // },
   });
 };
 
@@ -40,3 +50,15 @@ const renderAvatar = (user) => {
     $(".text-avatar").html(firstName);
   }
 };
+// 退出登录
+$("#btnLogout").click(() => {
+  layui.layer.confirm(
+    "确定退出登录？", { icon: 3, title: "" },
+    function(index) {
+      // 清空本地存储里面的 token
+      localStorage.removeItem("token");
+      // 重新跳转到登录页面
+      location.href = "/login.html";
+    }
+  );
+});
